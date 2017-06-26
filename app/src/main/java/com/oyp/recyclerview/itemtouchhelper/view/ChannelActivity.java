bddd11d9b4241b5d3ac9d63b710524f28a77e651
ouyangpeng <ouyangpeng@oaserver.dw.gdbbk.com>
2017-06-26 19:34:36 +0800

    更改修改Channel的布局页面，修复item移动动画不对劲的bug

--- app/src/main/java/com/oyp/recyclerview/itemtouchhelper/view/ChannelActivity.java
+++ app/src/main/java/com/oyp/recyclerview/itemtouchhelper/view/ChannelActivity.java
@@ -34,7 +34,7 @@ public class ChannelActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recycler_view);
+        setContentView(R.layout.activity_channel);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.rv);
         init();
